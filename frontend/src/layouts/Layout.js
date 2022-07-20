a0994409ca8d0c0e0499e9fe9d1b805a315508a8
damiankuchta <damkuch@gmail.com>
2022-07-20 15:55:43 +0100

    basic structure

---
+++ frontend/src/layouts/Layout.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Sidebar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
+import Main from "./Main/Main";
+
+export default function Layout({children}) {
+
+    return (
+        <React.Fragment>
+            <Main>{children}</Main>
+            <Sidebar/>
+            <Footer/>
+        </React.Fragment>
+    )
+}
+
---
+++ frontend/src/layouts/Main/Main.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Main({children}) {
+
+    return (
+        <React.Fragment>
+            {children}
+        </React.Fragment>
+    )
+}
