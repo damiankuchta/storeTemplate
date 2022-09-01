86f03cb36f8d6b4e9cdb83cd730a2ba654517bf8
damiankuchta <damkuch@gmail.com>
2022-09-02 00:42:46 +0100

    whoops

--- frontend/src/layouts/Main/Main.js
+++ frontend/src/layouts/Main/Main.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Product from "../../pages/Product"
+import Home from "../../pages/Home";
 
 export default function Main() {
 
     return (
-        // <ShowCaseList/>
-        <Product/>
+        <Home/>
+        // <Product/>
     )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,19 +11,19 @@ export default function Home() {
             {/*3 and 1 column gird*/}
             <Grid display={{"xs": "flex", "md": "none", "lg": "flex"}} container>
                 <Grid xs={12} md={6} lg={4}>
-                    {showListData.slice(productsList.length / 3 * 2).map(({image}, index) => {
+                    {productsList.slice(productsList.length / 3 * 2).map(({image}, index) => {
                         return <ShowcaseProduct src={image} isBigger={(index + 1) % 3 === 0}/>
                     })}
                 </Grid>
 
                 <Grid xs={12} md={6} lg={4}>
-                    {showListData.slice(productsList.length / 3, (data.length / 3) * 2).map(({image}, index) => {
+                    {productsList.slice(productsList.length / 3, (productsList.length / 3) * 2).map(({image}, index) => {
                         return <ShowcaseProduct src={image} isBigger={(index + 2) % 3 === 0}/>
                     })}
                 </Grid>
 
                 <Grid xs={12} md={6} lg={4}>
-                    {showListData.slice(0, productsList.length / 3).map(({image}, index) => {
+                    {productsList.slice(0, productsList.length / 3).map(({image}, index) => {
                         return <ShowcaseProduct src={image} isBigger={(index + 3) % 3 === 0}/>
                     })}
                 </Grid>
@@ -32,13 +32,13 @@ export default function Home() {
             {/*2 column grid*/}
             <Grid display={{"xs": "none", "md": "flex", "lg": "none"}} container>
                 <Grid xs={12} md={6}>
-                    {showListData.slice(0, productsList.length / 2).map(({image}, index) => {
+                    {productsList.slice(0, productsList.length / 2).map(({image}, index) => {
                         return <ShowcaseProduct src={image} isBigger={(index + 1) % 3 === 0}/>
                     })}
                 </Grid>
 
                 <Grid showlistData={12} md={6}>
-                    {showListData.slice(productsList.length / 2).map(({image}, index) => {
+                    {productsList.slice(productsList.length / 2).map(({image}, index) => {
                         return <ShowcaseProduct src={image} isBigger={(index + 2) % 3 === 0}/>
                     })}
                 </Grid>
