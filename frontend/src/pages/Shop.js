1b44ebbe64fc0db65b7ce14ab4a86e42d2012194
damiankuchta <damkuch@gmail.com>
2022-09-09 02:35:07 +0100

    shop inital structure

--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -1,10 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
 
 export default function Shop() {
 
+    const [filters, setFilters] = useState({
+        brands: [],
+        category: "",
+        colors:  [],
+        price_range: [],
+    })
+
+    const [displayType, setDisplayType] = useState("tiles")
+    const [sortBy, setSortBy] = useState("Date")
+    const [view, setView] = useState(12)
+
+
     return (
         <React.Fragment>
-
+        {/* dislay amount of pages*/}
+        {/* filters menu */}
+        {/* display/sort menu */}
+        {/* product component*/}
+        {/* pages */}
         </React.Fragment>
     )
 }
